567529eb258e67d93517f823ee5af98129f37a81
Smiril <Smiril@users.noreply.github.com>
2015-08-15 23:05:36 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://websms.at/images/websms/developer/sdks/CPP-SMS-Toolkit-1.0.0.zip
+unzip -d SDK CPP-SMS-Toolkit-1.0.0.zip
+cd SDK/
+make && sudo make install
+cd ../
+sudo make
