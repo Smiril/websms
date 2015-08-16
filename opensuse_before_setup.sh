f4508c53348f9913d9fd788bfe3affae520e5b64
Smiril <Smiril@users.noreply.github.com>
2015-08-16 16:52:01 +0200

    Create opensuse_before_setup.sh

---
+++ opensuse_before_setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo zypper install libjansson4 libjansson4-32bit libjansson-devel curl libcurl4 libcurl4-32bit libcurl-devel patterns-openSUSE-devel_basis patterns-openSUSE-devel_C_C++
