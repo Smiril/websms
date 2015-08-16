0f7e77a05ee9f335bc7b824401deeb9ecb550f1a
Smiril <Smiril@users.noreply.github.com>
2015-08-16 21:20:46 +0200

    Update main-c.cc

--- main-c.cc
+++ main-c.cc
@@ -158,7 +158,7 @@ void sendsms(std::string a1,std::string b1,std::string c1,std::string d1,std::st
     
       }catch (const Exception& e) {
         // Handle exceptions.
-	fprintf(stderr,"\x1B[39m\n			-=[ERROR]=-\n		Message: %s \n		To Number: %s\n\x1B[31m		%s\n\x1B[39m \n",(char *)b1.c_str(),(char *)a1.c_str(),e.What());
+	fprintf(stderr,"\x1B[39m\n			-=[ERROR]=-\n		Message: %s \n		To Number: %s\n		 -=[ \x1B[31m%s\x1B[39m ]=- \n",(char *)b1.c_str(),(char *)a1.c_str(),e.What());
 	//fprintf(stderr,"Status message: \x1B[31m %s\n\x1B[39m  \n", e.What());
 	}
       
