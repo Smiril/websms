a2e24853d5ddf5ef8f10ccec7cfb83ee669ee1bf
Smiril <Smiril@users.noreply.github.com>
2015-08-17 01:50:55 +0200

    Update unbuntu_installer.sh

--- unbuntu_installer.sh
+++ unbuntu_installer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt-get install build-essential libcurl4-openssl-dev libjansson-dev libgtk2.0-0 libglib2.0-0 libgdk-pixbuf2.0-0 libcairo2 libc6 git
+cd src/
 sh setup.sh
-
+cd ../
