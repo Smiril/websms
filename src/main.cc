48477f0c0a4072071baed31fc88f2f8c4c983074
Smiril <Smiril@users.noreply.github.com>
2015-08-18 01:02:03 +0200

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
     
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "websms 1.0.1.5");
+    gtk_window_set_title(GTK_WINDOW(window), "websms 1.2");
     gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
