100f02fee023975f433c857785422a351f83749f
Smiril <Smiril@users.noreply.github.com>
2015-08-18 01:47:14 +0200

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -4,7 +4,9 @@ if [  -f CPP-SMS-Toolkit-1.0.0.zip ]; then
 fi
 if [ ! -f CPP-SMS-Toolkit-1.0.0.zip ]; then
 wget https://websms.at/images/websms/developer/sdks/CPP-SMS-Toolkit-1.0.0.zip
+if [ ! -f SDK ]; then
 unzip -d SDK CPP-SMS-Toolkit-1.0.0.zip
+fi
 fi
 cd SDK/
 make && sudo make install
