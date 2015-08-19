e1cb3478fe1ed819b2d3991be61a18527e0bbd67
Smiril <Smiril@users.noreply.github.com>
2015-08-19 19:51:38 +0200

    Update W32_x64_opensuse_build.sh

--- src/W32_x64_opensuse_build.sh
+++ src/W32_x64_opensuse_build.sh
@@ -6,7 +6,7 @@ fi
 if [ ! -f libmpc2-0.8.2-12.1.6.x86_64.rpm ]; then
 wget ftp://mirror.switch.ch/pool/4/mirror/opensuse/opensuse/distribution/12.1/repo/oss/suse/x86_64/libmpc2-0.8.2-12.1.6.x86_64.rpm
 fi
-if [ ! -f gtk+-3.14.15.tar.xz ]; then
+if [ ! -f mingw32-cross-gcc-c++-4.8.1-11.3.x86_64.rpm ]; then
 wget ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/GNOME:/Apps:/Evince:/Windows:/3.6/openSUSE_12.2/x86_64/mingw32-cross-gcc-c++-4.8.1-11.3.x86_64.rpm
 fi
 
