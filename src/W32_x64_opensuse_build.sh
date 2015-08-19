b8ff354ed0cef136646b7626c6467d847453caef
Smiril <Smiril@users.noreply.github.com>
2015-08-19 20:38:31 +0200

    Update W32_x64_opensuse_build.sh

--- src/W32_x64_opensuse_build.sh
+++ src/W32_x64_opensuse_build.sh
@@ -6,14 +6,16 @@ fi
 if [ ! -f libmpc2-0.8.2-12.1.6.x86_64.rpm ]; then
 wget ftp://mirror.switch.ch/pool/4/mirror/opensuse/opensuse/distribution/12.1/repo/oss/suse/x86_64/libmpc2-0.8.2-12.1.6.x86_64.rpm
 fi
-if [ ! -f mingw32-cross-gcc-c++-4.8.1-11.3.x86_64.rpm ]; then
-wget ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/GNOME:/Apps:/Evince:/Windows:/3.6/openSUSE_12.2/x86_64/mingw32-cross-gcc-c++-4.8.1-11.3.x86_64.rpm
+if [ ! -f mingw32-cross-gcc-5.1.0-10.10.x86_64.rpm ]; then
+wget http://download.opensuse.org/repositories/windows:/mingw:/win32/openSUSE_13.2/x86_64/mingw32-cross-gcc-5.1.0-10.10.x86_64.rpm
 fi
+
 if [ -f libmpc2-0.8.2-12.1.6.x86_64.rpm ]; then
 sudo rpm -iv libmpc2-0.8.2-12.1.6.x86_64.rpm
 fi
-if [ -f mingw32-cross-gcc-c++-4.8.1-11.3.x86_64.rpm ]; then
-sudo rpm -iv mingw32-cross-gcc-c++-4.8.1-11.3.x86_64.rpm
+if [ -f mingw32-cross-gcc-5.1.0-10.10.x86_64.rpm ]; then
+sudo rpm -iv mingw32-cross-gcc-5.1.0-10.10.x86_64.rpm
 fi
-make websms-exe websms-console-exe
+
+make websms-exe-x64 websms-console-exe-x64 websms-exe-W32 websms-console-exe-W32
 
