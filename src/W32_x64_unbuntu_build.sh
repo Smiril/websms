863d8c861c10bdfb1104292a0b0e7db5377dfd77
Smiril <Smiril@users.noreply.github.com>
2015-08-19 21:58:49 +0200

    Update W32_x64_unbuntu_build.sh

--- src/W32_x64_unbuntu_build.sh
+++ src/W32_x64_unbuntu_build.sh
@@ -3,7 +3,7 @@ if [ -f GTKWIN32/ ]; then
     continue
 fi
 
-sudo apt-get install mingw-w64 mingw-w64-tools mingw-w64-common mingw-w64-i686-dev mingw-w64-x86-64-dev
+sudo apt-get install mingw-w64 mingw-w64-tools mingw-w64-common mingw-w64-i686-dev mingw-w64-x86-64-dev g++-mingw-w64-i686
 
 make websms-exe-x64 websms-console-exe-x64 websms-exe-W32 websms-console-exe-W32
 
