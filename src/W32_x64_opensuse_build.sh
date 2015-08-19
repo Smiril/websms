442e9f71bf0fd64d4db820bff09db14dbd5e362d
Smiril <Smiril@users.noreply.github.com>
2015-08-19 22:21:24 +0200

    Update W32_x64_opensuse_build.sh

--- src/W32_x64_opensuse_build.sh
+++ src/W32_x64_opensuse_build.sh
@@ -4,7 +4,7 @@ if [ -f GTKWIN32/ ]; then
 fi
 
 
-sudo zypper install mingw32-cross-gcc mingw32-cross-g++ mingw32-cross-gcc-bootstrap mingw32-runtime mingw32-winpthreads
+sudo zypper install mingw32-cross-gcc mingw32-cross-gcc-bootstrap mingw32-runtime
 
 
 make websms-exe-x64 websms-console-exe-x64 websms-exe-W32 websms-console-exe-W32
