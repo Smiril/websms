6acaa7d90d9428df004cb079f267001544dc3488
Smiril <Smiril@users.noreply.github.com>
2015-08-19 23:58:06 +0200

    Update unbuntu_installer.sh

--- unbuntu_installer.sh
+++ unbuntu_installer.sh
@@ -3,14 +3,13 @@
 sudo apt-get install build-essential libcurl4-openssl-dev libjansson-dev libgtk2.0-0 libglib2.0-0 libgdk-pixbuf2.0-0 libcairo2 libc6 git
 cd src/
 sh setup.sh
-cd ../
 echo "Make Windows Binary?(only Unbuntu x64) Do that? [Y,n]"
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
         echo "doing that ..."
-        cd src/
         sh W32_x64_unbuntu_build.sh
         cd ../
 else
         echo "don't doing that ..."
+        cd ../
 fi
