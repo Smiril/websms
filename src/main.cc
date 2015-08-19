c72cd2eaf4ebf4ae4350a3233ca58b7d6e3cea2a
Smiril <Smiril@users.noreply.github.com>
2015-08-19 15:20:41 +0200

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -18,6 +18,36 @@ std::string a = "";
 std::string b = "";
 std::string c = "";
 std::string d = "";
+std::string dx = "";
+std::string dd = "";
+std::string aa = "Send as Flash SMS";
+std::string bb = "Send with Priority";
+
+GtkWidget *CreateCheckBox (GtkWidget *box, char *szLabel)
+{
+    GtkWidget *check;
+
+    /* --- Get the check button --- */
+    check = gtk_check_button_new_with_label (szLabel);
+
+    /* --- Pack the checkbox into the vertical box (box). --- */
+    gtk_box_pack_start (GTK_BOX (box), check, FALSE, FALSE, 2);
+
+    /* --- Show the widget --- */
+    gtk_widget_show (check);
+
+    return (check);
+}
+
+void flashsms(GtkWidget *, gpointer *)
+{
+  dx = (char *)"y";
+}
+
+void prioritysms(GtkWidget *, gpointer *)
+{
+  dd = (char *)"y";
+}
 
 void getnumberx(GtkButton *,gpointer dash11)
 {
@@ -50,9 +80,39 @@ void change_text(GtkButton *,gpointer numberx)
 }  
 
 void sendsms()
-{     int value = 0;
+{
+      const char *pch;
+      int value = 0;
+      pch = strtok ((char *)a.c_str(),",");
+      // 99 phonenumbers max per sms sending sequence
+      for(int f = 0;f < 99;f++){
+      while (pch != NULL)
+      {
       SmsClient client(c.c_str(), d.c_str(), "https://api.websms.com/json"); // <<< Websms.at specific SDK Client
-      TextMessage message(strtol(a.c_str(),NULL,value), UTF8((char *)b.c_str())); // <<< Websms.at specific SDK Transmission Format
+      TextMessage message(strtol(pch,NULL,value), UTF8((char *)b.c_str())); // <<< Websms.at specific SDK Transmission Format
+      printf("Outgoing SMS ... %lu\n",strtol(pch,NULL,value));
+      
+      // TextMessage recipient_address_list();
+      // God's will welcome but User vote and got some Sympathy with the Devil ...
+      std::string god = "y";
+      std::string devil = "y";
+      if(dx.c_str() == god && dd.c_str() != devil)
+      {
+      TextMessage send_as_flash_sms();
+      printf("\x1B[32mSending as \x1B[33mFlash\x1B[32m SMS with \x1B[33mLow Priority!\x1B[39m\n");
+      }
+      else if(dx.c_str() == god && dd.c_str() == devil){
+      TextMessage send_as_flash_sms();
+      printf("\x1B[32mSending as \x1B[33mFlash\x1B[32m SMS with \x1B[33mHigh Priority!\x1B[39m\n");
+      TextMessage set_priority();
+      }
+      else if(dx.c_str() != god && dd.c_str() == devil){
+      printf("\x1B[32mSending as \x1B[33mRegular\x1B[32m SMS with \x1B[33mHigh Priority!\x1B[39m\n");
+      TextMessage set_priority();
+      }
+      else{
+      printf("\x1B[32mSending as \x1B[33mRegular\x1B[32m SMS with \x1B[33mLow Priority!\x1B[39m\n");
+      }
       
       try {
         // Send the message.
@@ -61,29 +121,31 @@ void sendsms()
                false);  // Test message?
 	GtkWidget *msgboxxx;
 	char *wexx = NULL;
-	wexx = g_strdup_printf("Message: %s \n\nTo Number: %lu \n\nStatus message: %s\nStatus code: %d\n",b.c_str(),strtol(a.c_str(),NULL,value),response.status_message(),response.status_code());
+	wexx = g_strdup_printf("Message: %s \n\nTo Number: %lu \n\nStatus message: %s\nStatus code: %d\n",b.c_str(),strtol(pch,NULL,value),response.status_message(),response.status_code());
         msgboxxx = gtk_message_dialog_new_with_markup(NULL,GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, wexx );
 	gtk_window_set_title(GTK_WINDOW(msgboxxx), "INFO");
 	gtk_dialog_run(GTK_DIALOG(msgboxxx));
 	gtk_widget_destroy( GTK_WIDGET(msgboxxx) );
-	fprintf(stderr,"Message: %s \n\nTo Number: %lu \n\n%s \n",b.c_str(),strtol(a.c_str(),NULL,value),response.status_message());
+	fprintf(stderr,"Message: %s \n\nTo Number: %lu \n\n%s \n",b.c_str(),strtol(pch,NULL,value),response.status_message());
     
       }catch (const Exception& e) {
         // Handle exceptions.	
 	GtkWidget *msgbox;
 	char *wex = NULL;
-	wex = g_strdup_printf("Message: %s \n\nTo Number: %lu \n\n%s ",b.c_str(),strtol(a.c_str(),NULL,value),e.What());
+	wex = g_strdup_printf("Message: %s \n\nTo Number: %lu \n\n%s ",b.c_str(),strtol(pch,NULL,value),e.What());
         msgbox = gtk_message_dialog_new_with_markup(NULL,GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, wex );
 	gtk_window_set_title(GTK_WINDOW(msgbox), "INFO");
 	gtk_dialog_run(GTK_DIALOG(msgbox));
 	gtk_widget_destroy( GTK_WIDGET(msgbox) );
-	fprintf(stderr,"Message: %s \n\nTo Number: %lu \n\n%s \n",b.c_str(),strtol(a.c_str(),NULL,value),e.What());
+	fprintf(stderr,"Message: %s \n\nTo Number: %lu \n\n%s \n",b.c_str(),strtol(pch,NULL,value),e.What());
       }
-      
+	pch = strtok (NULL, ",");
+      } // while end
+      } // for end
 }
 
 int main(int argc, char *argv[]) {
-    freopen( "/var/log/Smiril-websms-error.log", "a+", stderr );
+    //freopen( "/var/log/Smiril-websms-error.log", "a+", stderr );
     
     GtkWidget *window;
     GtkWidget *vbox;
@@ -93,17 +155,19 @@ int main(int argc, char *argv[]) {
     GtkWidget *pass;
     GtkWidget *number;
     GtkWidget *msg;
+    GtkWidget *check1;
+    GtkWidget *check2;
     gtk_init(&argc, &argv);
     
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "websms 1.2");
+    gtk_window_set_title(GTK_WINDOW(window), "Smiril-websms 1.3");
     gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
     //gtk_window_set_icon(GTK_WINDOW(window), gtk_xpm);
     gtk_window_has_toplevel_focus(GTK_WINDOW(window));
     
-    vbox = gtk_vbox_new(FALSE, 5);
+    vbox = gtk_vbox_new(FALSE, 2);
     gtk_container_add(GTK_CONTAINER(window), vbox);
  
     label = gtk_label_new("Give me Data!");
@@ -134,6 +198,15 @@ int main(int argc, char *argv[]) {
     gtk_entry_set_invisible_char(GTK_ENTRY(pass),'*');
     gtk_box_pack_start(GTK_BOX(vbox), pass, FALSE, FALSE, 0);
     
+    check1 = CreateCheckBox (vbox, (char *)aa.c_str());
+    gtk_box_pack_start(GTK_BOX(vbox), check1, FALSE, FALSE, 0);
+    gtk_toggle_button_set_state (GTK_TOGGLE_BUTTON (check1), TRUE);
+    g_signal_connect(GTK_OBJECT(check1), "clicked", GTK_SIGNAL_FUNC(flashsms), NULL);
+    check2 = CreateCheckBox (vbox, (char *)bb.c_str());
+    gtk_box_pack_start(GTK_BOX(vbox), check2, FALSE, FALSE, 0);
+    gtk_toggle_button_set_state (GTK_TOGGLE_BUTTON (check2), TRUE);
+    g_signal_connect(GTK_OBJECT(check2), "clicked", GTK_SIGNAL_FUNC(prioritysms), NULL);
+    
     button = gtk_button_new_with_label("Send");
     gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
     //button2 = gtk_button_new_with_label("Reset");
@@ -151,6 +224,8 @@ int main(int argc, char *argv[]) {
     g_signal_connect(button, "clicked", G_CALLBACK(getmsgx), msg);
     g_signal_connect(button, "clicked", G_CALLBACK(getuserx), user);
     g_signal_connect(button, "clicked", G_CALLBACK(getpassx), pass);
+    g_signal_connect(button, "clicked", G_CALLBACK(flashsms), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(prioritysms), NULL);
     g_signal_connect(button, "clicked", G_CALLBACK(sendsms), NULL);
     
     gtk_widget_show_all(window);
