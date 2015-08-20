d83abd57c84b6e8a960c364d75c0761d3aef93e4
Smiril <Smiril@users.noreply.github.com>
2015-08-20 21:12:39 +0200

    Update W32_x64_unbuntu_build.sh

--- src/W32_x64_unbuntu_build.sh
+++ src/W32_x64_unbuntu_build.sh
@@ -5,5 +5,8 @@ fi
 
 sudo apt-get install mingw-w64 mingw-w64-tools mingw-w64-common mingw-w64-i686-dev mingw-w64-x86-64-dev g++-mingw-w64-i686
 
-make websms-exe-x64 websms-console-exe-x64 websms-exe-W32 websms-console-exe-W32
+make websms-exe-x64
+make websms-console-exe-x64
+make websms-exe-W32
+make websms-console-exe-W32
 
