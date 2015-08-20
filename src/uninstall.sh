93211553b2f7d1977aa5476d4cdb0e950f5a7168
Smiril <Smiril@users.noreply.github.com>
2015-08-20 16:48:22 +0200

    Update uninstall.sh

--- src/uninstall.sh
+++ src/uninstall.sh
@@ -7,6 +7,14 @@ cd ../
 rm -rf SDK/
 rm -rf CPP-SMS-Toolkit-1.0.0.zip
 rm -rf GTKWIN32/
+rm -rf GTKWIN64/
+rm -rf GLIBWIN32/
+rm -rf GLIBWIN64/
 rm -rf gtk+-dev_2.24.10-1_win32.zip
+rm -rf gtk+-dev_3.6.4-1_win64.zip
+rm -rf glib-dev_2.34.3-1_win32.zip
+rm -rf glib-dev_2.34.3-1_win64.zip
 rm -rf GTKLINUX/
 rm -rf gtk+-2.24.28.tar.xz
+rm -rf GLIBLINUX/
+rm -rf glib-2.44.0.tar.xz
