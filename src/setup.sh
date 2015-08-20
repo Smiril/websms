9889779d6f2415be46728b129e5e28d8f9daf1a1
Smiril <Smiril@users.noreply.github.com>
2015-08-20 16:02:39 +0200

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -9,6 +9,9 @@ fi
 if [ ! -f SDK ]; then
 unzip -d SDK CPP-SMS-Toolkit-1.0.0.zip
 fi
+if [ ! -f glib-dev_2.34.3-1_win32.zip ]; then
+wget http://win32builder.gnome.org/packages/3.6/glib-dev_2.34.3-1_win32.zip
+fi
 if [ ! -f gtk+-2.24.28.tar.xz ]; then
 wget http://ftp.gnome.org/pub/gnome/sources/gtk+/2.24/gtk+-2.24.28.tar.xz
 fi
@@ -20,6 +23,9 @@ wget http://ftp.gnome.org/pub/gnome/binaries/win32/gtk+/2.24/gtk+-dev_2.24.10-1_
 fi
 if [ ! -f GTKWIN32 ]; then
 unzip -d GTKWIN32 gtk+-dev_2.24.10-1_win32.zip
+fi
+if [ ! -f GLIBWIN32 ]; then
+unzip -d GLIBWIN32 glib-dev_2.34.3-1_win32.zip
 fi
 
 cd SDK/
@@ -34,7 +40,7 @@ cd GTKLINUX/
 fi
 if [ -f gtk+-2.24.28 ]; then
     cd gtk+-2.24.28
-    sh configure
+    sh ./configure
     make && sudo make install
     cd ../
 fi
